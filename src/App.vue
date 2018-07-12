a35927138e81ba0f02e60db738ad0c91551ae42e
zhu <964182377@qq.com>
2018-07-12 12:51:13 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <x-header >
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import XHeader from "vux/src/components/x-header/index";
+
+export default {
+  components: {XHeader},
+  name: 'app'
+}
+</script>
+
+<style lang="less">
+@import '~vux/src/styles/reset.less';
+
+body {
+  background-color: #fbf9fe;
+}
+</style>
---
+++ src/components/ui/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="position: fixed;background: black">
+    <p>
+    </p>
+  </div>
+</template>
+
+<script>
+  import ViewBox from "vux/src/components/view-box/index";
+
+  export default {
+    components: {ViewBox},
+    name: "index"
+  }
+</script>
+<style>
+  @import "../../css/mycss.css";
+</style>
+<style scoped>
+
+</style>
